89eea556f9c71715e4f338a656fcbab58a8d59f5
Steve Griff <ssg-sas@outlook.com>
2024-02-01 12:44:49 +0000

    added incrementing values to CAN transmission, also changed LED logic (1 per sec)

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,19 @@ const int canSpeed = 500000;  // 500 kbps
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can2;
 CAN_message_t msg;
 
+int LED_State = 0;
+int counter_ms = 0;
+
+//Used for CAN counter increments
+union _myunion
+{
+  /* data */
+  uint32_t u32data;
+  uint8_t u8data[4];
+  float f32;
+}
+myunion;
+
 void setup() {
   Serial.begin(115200);
   //Serial.println("Hello world");
@@ -21,25 +34,41 @@ void setup() {
 }
 
 void loop() {
+
+  if (++counter_ms >= 1000) {
+    counter_ms = 0;
+  }
+
   // Create a CAN message
   CAN_message_t canMsg;
   canMsg.id = 0x123;  // CAN ID
   canMsg.len = 8;     // Message length (8 bytes)
   
   // Fill data bytes with some values
-  for (int i = 0; i < 8; i++) {
-    canMsg.buf[i] = i;
-  }
-  
-  // Send the CAN message
-  Can2.write(canMsg);
+//  for (int i = 0; i < 8; i++) {
+//    canMsg.buf[i] = i;
+//  }
 
   //Serial.println("SG Test");
 
+  if (counter_ms == 0) {
   // set the LED on
   digitalWrite(ledPin, HIGH);
+    // Send the CAN message
 
-    if ( Can2.read(msg) ) {
+  myunion.u32data++;
+  canMsg.buf[0] = myunion.u8data[0];
+  canMsg.buf[1] = myunion.u8data[1];
+  canMsg.buf[2] = myunion.u8data[2];
+  canMsg.buf[3] = myunion.u8data[3];
+  canMsg.buf[4] = canMsg.buf[5] = canMsg.buf[6] = canMsg.buf[7] = 0x00;  
+  Can2.write(canMsg);
+  } 
+  if (counter_ms == 100) {
+  digitalWrite(ledPin, LOW);
+  }
+  /*
+    while ( Can2.read(msg) ) {
     Serial.print("CAN1 "); 
     Serial.print("MB: "); Serial.print(msg.mb);
     Serial.print("  ID: 0x"); Serial.print(msg.id, HEX );
@@ -51,13 +80,7 @@ void loop() {
     }
     Serial.print("  TS: "); Serial.println(msg.timestamp);
   }
-
+*/
   // Wait for one second before sending the next frame
-  delay(500);
-
-  // set the LED off
-  digitalWrite(ledPin, LOW);
-
-  // Wait for one second before sending the next frame
-  delay(500);
+  delay(1);
 }
