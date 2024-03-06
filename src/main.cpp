a98c83270365933918d2b1d88278b4db88bd838f
Steve Griff <ssg-sas@outlook.com>
2024-03-06 09:23:23 +0000

    added TimerStatus to help with debounce on hw interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 #define HWSERIAL Serial2
 #define GNSSSERIAL Serial8
 
-//LED, Buzzer, Digout
+// LED, Buzzer, Digout
 const int ledPin = 14; // Dig signal out on screw block terminal
 const int ledPin2 = 23; // Buzzer & Red LED
 const int ledPin3 = 33; // White LED Only
+
+// Timer tracker
+int TimerStatus = 1;
 
 volatile bool sendCanMsg = false;
 volatile uint32_t canCounter = 0;
@@ -22,6 +25,11 @@ CAN_message_t msg;
 void blinkLED() {
   static int LED_Count = 0;
   LED_Count++;
+
+  if (LED_Count >= 30) {
+    TimerStatus = 0;
+  }
+
   if (LED_Count >= 101) {
     Timer1.stop();
     digitalWrite(ledPin, LOW);
@@ -32,23 +40,17 @@ void blinkLED() {
 }
 
 void myInterrupt() {
-  static int SerCount = 1;
-  if (!sendCanMsg) {
+  if (!sendCanMsg && TimerStatus == 0 ) {
     sendCanMsg = true;
     Timer1.start();
+    TimerStatus = 1;
     digitalWrite(ledPin, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, HIGH);
-    // Increment Serial message 
-    HWSERIAL.print("PPS Time Stamp "); 
-    HWSERIAL.println(SerCount++);
   }
 }
 
 void setup() {
-  Timer1.initialize(1000); // MicroSecs = 1ms
-  Timer1.attachInterrupt(blinkLED); // blinkLED function 1ms interrupt
-  Timer1.stop(); // Stop the timer!
   Serial.begin(115200);
   HWSERIAL.begin(115200);
   Can2.begin();
@@ -57,6 +59,9 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(28), myInterrupt, RISING);
+  Timer1.initialize(1000); // MicroSecs = 1ms
+  Timer1.attachInterrupt(blinkLED); // blinkLED function 1ms interrupt
+  Timer1.start(); // Start the timer!
 }
 
 void loop() {
@@ -71,6 +76,11 @@ void loop() {
     canMsg.buf[3] = canCounter & 0xFF;
     canMsg.buf[4] = canMsg.buf[5] = canMsg.buf[6] = canMsg.buf[7] = 0x00;  
     Can2.write(canMsg);
+
+    // Serial message 
+    HWSERIAL.print("PPS Time Stamp ");
+    HWSERIAL.println(canCounter);
+
     sendCanMsg = false;
   }
 
